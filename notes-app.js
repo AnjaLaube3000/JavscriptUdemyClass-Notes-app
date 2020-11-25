4c311baa6c7e695794da6067f7f03b5852e183cb
Anja Laube <mail@anjalaube.com>
2020-11-25 11:12:45 +0100

    first commit

---
+++ notes-app.js
@@ -0,0 +1,64 @@
+// import timeRelative from '/dayjs/plugin/timeRelative'
+// dayjs.extend(timeRelative)
+// // timeRelative plugin for day.js-library
+
+// checking for existing data saved
+let notes = loadSavedData()
+
+const filters = {
+  searchText: '',
+  sortBy: 'byEdited'
+}
+
+//render all notes
+renderedNotes(notes, filters)
+
+// filter notes
+document.querySelector('#search-text').addEventListener('input', (e) => {
+  filters.searchText = e.target.value
+  renderTodos(todos, filters)
+})
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+  const id = uuidv4()
+  const timestamp = moment().valueOf()
+  notes.push({
+    id: id,
+    title: '',
+    body: '',
+    createdAt: timestamp,
+    updatedAt: timestamp
+  })
+  location.assign(`/edit.html#${id}`)
+  saveData(notes)
+  renderedNotes(notes, filters)
+})
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+  filters.searchText = e.target.value
+  renderedNotes(notes, filters)
+})
+
+// drowpdown menu
+document.querySelector('#sorting-option').addEventListener('change', (e) => {
+  filters.sortBy = e.target.value
+  renderedNotes(notes, filters)
+})
+
+// sync data from the mainpage to the edit page
+window.addEventListener('storage', (e) => {
+  if(e.key === notes) {
+    notes = JSON.parse(e.newValue)
+    renderedNotes(notes, filters)
+  }
+})
+
+// DAY JS
+// //setup day.js library
+// const now = dayjs()
+
+// const myBirthday = now.subtract(34, 'year').subtract(8, 'month').add(4, 'days')
+// console.log(myBirthday.format('MMM D,YYYY'))
+
+
+// console.log(myBirthday.fromNow())
---
+++ notes-function.js
@@ -0,0 +1,117 @@
+//library to create unique ids for each note
+uuidv4()
+
+// load data
+const loadSavedData = () => {
+  const notesJSON = localStorage.getItem('notes')
+
+  if (notesJSON !== null) {
+    return JSON.parse(notesJSON)
+  } else {
+    return []
+  }
+}
+
+// save data
+const saveData = (notes) => {
+  localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// sort notes by one of the options in dropdown menu
+const sortNotes = (notes, sortBy) => {
+  if (sortBy === 'byEdited') {
+    return notes.sort ((a,b) => {
+      if (a.updatedAt > b.updatedAt) {
+        return -1
+      } else if (a.updatedAt < b.updatedAt) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  } else if (sortBy === 'byCreated') {
+    return notes.sort((a, b) => {
+      if (a.createdAt > b.createdAt) {
+        return -1
+      } else if (a.createdAt < b.createdAt) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  } else if (sortBy === 'alphabetical') {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1
+      } else if ( a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  } else {
+    return notes
+  }
+}
+
+
+//render all notes
+const renderedNotes = (notes, filters) => {
+  notes = sortNotes(notes, filters.sortBy)
+  let filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+
+  document.querySelector('#notes').innerHTML = ''
+
+  filteredNotes.forEach((note)  => {
+    document.querySelector('#notes').appendChild(generateNoteDOM(note))
+  })
+}
+
+// remove item from list of notes via index
+const removeNote = (id) => {
+  const index = notes.findIndex((note) => note.id === id)
+  if (index > -1) {
+    notes.splice(index, 1)
+  }
+}
+
+// generate the DOM structure for a note
+const generateNoteDOM = (note) => {
+  let p = document.createElement('div')
+  const textElement = document.createElement('a')
+  const button = document.createElement('button')
+  // const createdAt = document.createElement('span')
+  // let updatedAt = document.createElement('span')
+
+  // set up remove button
+  button.textContent = 'x'
+  p.appendChild(button)
+  button.addEventListener('click', () => {
+    removeNote(note.id)
+    saveData(notes)
+    renderedNotes(notes, filters)
+  })
+
+  // set up title
+  if (note.title.length > 0) {
+    textElement.textContent = note.title
+  } else {
+    textElement.textContent = 'Untitled'
+  }
+  textElement.setAttribute('href', `/edit.html#${note.id}`)
+  p.appendChild(textElement)
+
+  // //created at
+  // createdAt.textContent = `Note created at ${note.createdAt}`
+  // p.appendChild(createdAt)
+
+  // //updated at
+  // updatedAt.textContent = `Last edited at ${note.updatedAt}`
+  // p.appendChild(updatedAt)
+
+  return p
+}
+
+
+// generate Last Edited Message
+const lastEditedMessage = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`
